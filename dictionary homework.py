c4d12e830e731274919678368b492fdab4e29cb4
umar <umarqadoos93@gmail.com>
2025-03-16 15:59:22 +0000

    dictionary homework

---
+++ dictionary homework.py
@@ -0,0 +1,13 @@
+meanings={
+    "noun": "a word used to identify any of a class of people, places, or things ",
+    "verb": "a word used to describe an action, state, or occurrence, and forming the main part of the predicate of a sentence, such as hear, become, happen",
+    "onomatapia": "the formation of a word from a sound associated with what is named (e.g. cuckoo, sizzle )."
+}
+
+print(meanings.keys())
+print(meanings.values())
+
+meanings["noun"]="a word that denotes a person, place, thing, or idea"
+print(meanings)
+
+
